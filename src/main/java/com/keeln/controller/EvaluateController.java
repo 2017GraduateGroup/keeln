4fcaf0411875d74f298eb6a5e2c23474c9e822b8
KarryCol <1003478213@qq.com>
2017-06-05 15:23:16 +0800

    bigfix: 评论接口

--- src/main/java/com/keeln/controller/EvaluateController.java
+++ src/main/java/com/keeln/controller/EvaluateController.java
@@ -1,27 +1,29 @@
 package com.keeln.controller;
 
+import com.keeln.domain.model.BizResult;
 import com.keeln.domain.model.EvaluateDO;
 import com.keeln.domain.query.EvaluateQuery;
 import com.keeln.manager.EvaluateManager;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
  * Created by Xianrui Ke on 2017/5/18.
  */
-@Controller
+@RestController
 @RequestMapping("evaluate")
 public class EvaluateController {
     @Autowired
     EvaluateManager evaluateManager;
 
     @RequestMapping("addEvaluate")
-    public void addEvaluate(@RequestParam String userId, @RequestParam String scenicRegionId, @RequestParam String content){
+    public BizResult addEvaluate(@RequestParam String userId, @RequestParam String scenicRegionId, @RequestParam String content){
+        BizResult bizResult = new BizResult();
         EvaluateDO evaluateDO = new EvaluateDO();
         if(StringUtils.isNotBlank(userId)){
             evaluateDO.setUserId(Long.valueOf(userId));
@@ -32,33 +34,59 @@ public class EvaluateController {
         if(StringUtils.isNotBlank(content)){
             evaluateDO.setContent(content);
         }
-        evaluateManager.insertSelective(evaluateDO);
+        Long result = evaluateManager.insertSelective(evaluateDO);
+        if(result > 0){
+            bizResult.setCode("1");
+            bizResult.setMessage("success");
+            return bizResult;
+        }
+        bizResult.setCode("0");
+        bizResult.setMessage("fail");
+        return bizResult;
     }
 
     @RequestMapping("deleteEvaluate")
-    public void deleteEvaluate(@RequestParam String userId, @RequestParam String evaluateId){
+    public BizResult deleteEvaluate(@RequestParam String userId, @RequestParam String evaluateId){
+        BizResult bizResult = new BizResult();
         if(StringUtils.isNotBlank(evaluateId)){
             EvaluateDO evaluateDO = evaluateManager.selectByPrimaryKey(Long.valueOf(evaluateId));
             //判断当前评论存在并且属于该用户，才可以删除
             if(null != evaluateDO){
                 if(evaluateDO.getUserId().equals(Long.valueOf(userId))){
-                    evaluateManager.deleteByPrimaryKey(evaluateDO);
+                   Integer result =  evaluateManager.deleteByPrimaryKey(evaluateDO);
+                   if(result > 0){
+                       bizResult.setCode("1");
+                       bizResult.setMessage("success");
+                       return bizResult;
+                   }
                 }
             }
         }
+        bizResult.setCode("0");
+        bizResult.setMessage("fail");
+        return bizResult;
     }
 
     @RequestMapping("updateEvaluate")
-    public void updateEvaluate(@RequestParam String userId, @RequestParam String evaluateId, @RequestParam String content){
+    public BizResult updateEvaluate(@RequestParam String userId, @RequestParam String evaluateId, @RequestParam String content){
+        BizResult bizResult = new BizResult();
         if(StringUtils.isNotBlank(evaluateId)){
             EvaluateDO evaluateDO = evaluateManager.selectByPrimaryKey(Long.valueOf(evaluateId));
             if(null != evaluateDO){
                 if(evaluateDO.getUserId().equals(Long.valueOf(userId)) && StringUtils.isNotBlank(content)){
                     evaluateDO.setContent(content);
-                    evaluateManager.updateByPrimaryKeySelective(evaluateDO);
+                    Integer result = evaluateManager.updateByPrimaryKeySelective(evaluateDO);
+                    if(result > 0){
+                        bizResult.setCode("1");
+                        bizResult.setMessage("success");
+                        return bizResult;
+                    }
                 }
             }
         }
+        bizResult.setCode("0");
+        bizResult.setMessage("fail");
+        return bizResult;
     }
 
     @RequestMapping("queryCurrScenicEvaluateList")
